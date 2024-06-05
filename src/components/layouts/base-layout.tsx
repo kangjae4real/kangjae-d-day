9d151f09cdff489bbd7d2dfcab5f70b6be6ef68a
Kangjae Choi <awpe89@gmail.com>
2024-06-05 18:35:04 +0900

    feat: add BaseLayout component code

--- src/components/layouts/base-layout.tsx
+++ src/components/layouts/base-layout.tsx
@@ -2,14 +2,24 @@ import React from "react";
 import { cva, VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
-const baseLayoutVariants = cva("");
+const baseLayoutVariants = cva("h-full min-h-dvh", {
+  variants: {
+    width: {
+      default: "w-full max-w-[1000px] mx-auto",
+      full: "w-full",
+    },
+  },
+  defaultVariants: {
+    width: "default",
+  },
+});
 
 interface BaseLayoutProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof baseLayoutVariants> {}
 
-const BaseLayout = ({ children, className, ...props }: BaseLayoutProps) => {
+const BaseLayout = ({ children, className, width, ...props }: BaseLayoutProps) => {
   return (
-    <div className={cn(baseLayoutVariants(), className)} {...props}>
-      <div>{children}</div>
+    <div className={cn(baseLayoutVariants({ width }), className)} {...props}>
+      <div className="h-full w-full">{children}</div>
     </div>
   );
 };
